dbfd87189fb73bae2040cae0d54f9bd26c3c7c32
Nelson Malbone <nhmalbone@icloud.com>
2024-12-23 18:09:20 -0500

    setting up for the conversion

--- tutorial/main.py
+++ tutorial/main.py
@@ -10,7 +10,11 @@ conversion = st.radio("Choose the conversion: ", ('USD to EUR', 'EUR to USD'))
 if conversion == 'USD to EUR':
     # intending to get euros from usd
     user_input_usd = st.number_input("Enter the amount in USD:")
+    # calculating the conversion
+    # result of conversion
+    euros = usd_to_euros()
 
 else:
     # intending to get usd from euros
     user_input_eur = st.number_input("Enter the amount in EUR:")
+    dollars = usd_to_euros()
