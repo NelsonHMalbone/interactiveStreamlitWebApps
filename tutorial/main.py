b0f33b1004dbc19966d30b960fefe9be667ed6f0
Nelson Malbone <nhmalbone@icloud.com>
2024-12-23 18:00:52 -0500

    import streamlit place a title in working on the conversions statements

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,1 +1,12 @@
-from tutorial import config
+# making a live converter for currency
+
+# from tutorial import config
+import streamlit as st
+
+
+st.title('Live Currency Converter')
+conversion = st.radio("Choose the conversion: ", ('USD to EUR', 'EUR to USD'))
+if conversion == 'USD to EUR':
+    pass
+elif conversion == 'EUR to USD':
+    pass
